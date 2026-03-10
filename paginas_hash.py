b37b875b916774c5220bf92ebdfb2bfac25885b0
loreslei <lorennaaguiardev@gmail.com>
2026-03-10 08:31:59 -0300

    creating hash classes

--- paginas_hash.py
+++ paginas_hash.py
@@ -9,9 +9,28 @@ class Bucket:
         self.data = []
         self.overflow = []
         
+    def hash_index(tuple):
+        h = 0
+        for char in tuple:
+            h = (h * 31 + ord(char)) % (2**32)
+        return h
+        
+    def insert_hash(self, tuple, value):
+        index = self.hash_index(tuple)
+        self.data[index].append((tuple, value))
+    
+    def search(self, tuple):
+        index = self.hash_index(tuple)
+        
+        for t, v in self.data[index]:
+            if t == tuple:
+                return v
+        return 
+    
+    
+        
 
 def paginate(words, tuples_per_page):
-    
 
     pages = []
     page_number = 1
@@ -26,3 +45,7 @@ def paginate(words, tuples_per_page):
         page_number += 1
 
     return pages
+
+
+
+
