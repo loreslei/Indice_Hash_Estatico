aa47fbc55445f01b74dc1981b4b9f2ea10919df8
Lorenna Aguiar Nunes <133262137+loreslei@users.noreply.github.com>
2026-03-22 15:55:16 -0300

    agora vai

--- web_main.py
+++ web_main.py
@@ -29,7 +29,10 @@ app = FastAPI(title="API de Busca em Banco de Dados", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000, https://front-end-hash.vercel.app"], 
+    allow_origins=[
+        "http://localhost:3000", 
+        "https://front-end-hash.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
